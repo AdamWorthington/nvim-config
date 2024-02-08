3a335d416edf0683922710b03a6ae67c59367e8c
adamworthington <aaworth111@gmail.com>
2024-02-08 00:42:55 -0800

    Added copilot and modified cmp configs

--- lua/aworthington/plugins/treesitter.lua
+++ lua/aworthington/plugins/treesitter.lua
@@ -4,11 +4,21 @@ return {{
     config = function ()
       local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go", "java", "json", "dockerfile" },
-          auto_install = true,
-          sync_install = false,
-          highlight = { enable = true },
+        configs.setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go", "java", "json", "dockerfile" },
+            auto_install = true,
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
         })
     end
- }}
+}}
